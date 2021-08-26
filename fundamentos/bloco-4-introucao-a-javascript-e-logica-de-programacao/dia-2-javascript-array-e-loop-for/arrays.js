03749b41284743c9e3ee7932b730f612d6a66fb2
Millene Galetti <mikagaletti1@gmail.com>
2021-08-26 18:48:12 -0300

    Exercicio 9-finalizado

--- fundamentos/bloco-4-introucao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/arrays.js
+++ fundamentos/bloco-4-introucao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/arrays.js
@@ -65,3 +65,8 @@ for(let index = 0;index <= 25;index +=1){
     array.push(index);
 }
 console.log(array);
+
+// Exercicio 9
+for(let index = 0; index <=25;index +=1){
+console.log(array[index] / 2);
+};
