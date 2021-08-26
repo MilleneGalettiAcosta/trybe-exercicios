1c71f2307cb54902920363d21aef859c0828e743
Millene Galetti <mikagaletti1@gmail.com>
2021-08-26 19:02:15 -0300

    Bonus 2-finalizado

--- fundamentos/bloco-4-introucao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus.js
+++ fundamentos/bloco-4-introucao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus.js
@@ -13,3 +13,15 @@ for (let index = 1; index < numbers.length; index += 1) {
 
   console.log(numbers);
 //   Bonus 2
+for (let index = 1; index < numbers.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+      if (numbers[index] > numbers[secondIndex]) {
+        let position = numbers[index];
+        numbers[index] = numbers[secondIndex];
+        numbers[secondIndex] = position;
+      }
+    }
+  }
+
+  console.log(numbers);
+//   Bonus 3
