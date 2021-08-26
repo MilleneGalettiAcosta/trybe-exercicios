475ff382b0dff6ab9badc7fdd0fb1f285324c331
Millene Galetti <mikagaletti1@gmail.com>
2021-08-26 08:03:49 -0300

    Exercicio 3-finalizado

--- fundamentos/bloco-4-introucao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introucao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -16,3 +16,10 @@ if(c>d) {
   console.log("O valor d é maior");
 }
 
+if(a>b && a>c) {
+    console.log("A é maior")
+} else if(b>a && b>c) {
+    console.log("B é maior")
+} else {
+    console.log("C é o maior")
+}
