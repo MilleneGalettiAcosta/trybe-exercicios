90b253898059ffe86003e57378ce6a633574e64f
Millene Galetti <mikagaletti1@gmail.com>
2021-08-27 13:36:18 -0300

    Exercicio 1-finalizado

---
+++ fundamentos/bloco-4-introucao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/logicaDeProgramacao.js
@@ -0,0 +1,11 @@
+// Exercicio 1
+let n = 5;
+let simbolo = "*";
+let linha = "";
+
+for(let i = 0; i < n; i += 1){
+    linha = linha + simbolo;
+};
+for(let i = 0; i < n; i += 1){
+    console.log(linha);
+};
