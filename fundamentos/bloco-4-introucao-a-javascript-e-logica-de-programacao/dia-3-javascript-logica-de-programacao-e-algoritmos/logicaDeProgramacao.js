fee32480ee221e97490dfc83181f702351cd5c1f
Millene Galetti <mikagaletti1@gmail.com>
2021-08-27 14:01:07 -0300

    Exercicio 3- finzaliado

--- fundamentos/bloco-4-introucao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/logicaDeProgramacao.js
+++ fundamentos/bloco-4-introucao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/logicaDeProgramacao.js
@@ -11,7 +11,25 @@ let linha = "";
 // };
 
 // Exercicio 2
-for(let i = 0; i < n; i += 1){
-    linha += simbolo;
+// for(let i = 0; i < n; i += 1){
+//     linha += simbolo;
+//     console.log(linha);
+// }; 
+
+// Exercicio 3
+let posicao = n 
+// controlar a posição/coluna em que os asteriscos serão impressos
+
+// criar laço de repetição de coluna/posicao que estamos na linha
+for(let ilinha = 0; ilinha <= n; ilinha += 1){
+    for(let iposicao = 0; iposicao <= n; iposicao += 1){
+        if(iposicao < posicao){
+           linha = linha + " "; 
+        } else {
+            linha += simbolo; 
+        }
+    }
     console.log(linha);
-}; 
+    linha = "";
+    posicao -= 1;  
+    }; 
