bb2cee81c08dc66206a491fd59138e3c86bf91fe
Millene Galetti <mikagaletti1@gmail.com>
2021-09-03 10:39:08 -0300

    Resolução de exercicio

--- fundamentos/bloco-4-introucao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/logicaDeProgramacao.js
+++ fundamentos/bloco-4-introucao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/logicaDeProgramacao.js
@@ -17,19 +17,25 @@ let linha = "";
 // }; 
 
 // Exercicio 3
-let posicao = n 
+// let posicao = n 
 // controlar a posição/coluna em que os asteriscos serão impressos
 
 // criar laço de repetição de coluna/posicao que estamos na linha
-for(let ilinha = 0; ilinha <= n; ilinha += 1){
-    for(let iposicao = 0; iposicao <= n; iposicao += 1){
-        if(iposicao < posicao){
-           linha = linha + " "; 
-        } else {
-            linha += simbolo; 
-        }
-    }
-    console.log(linha);
-    linha = "";
-    posicao -= 1;  
+// for(let ilinha = 0; ilinha <= n; ilinha += 1){
+//     for(let iposicao = 0; iposicao <= n; iposicao += 1){
+//         if(iposicao < posicao){
+//            linha = linha + " "; 
+//         } else {
+//             linha += simbolo; 
+//         }
+//     }
+//     console.log(linha);
+//     linha = "";
+//     posicao -= 1;  
+//     }; 
+
+// Exercicio 4 
+for(let i = 0; i < n; i += 1){
+        linha += simbolo;
+        console.log(linha);
     }; 
