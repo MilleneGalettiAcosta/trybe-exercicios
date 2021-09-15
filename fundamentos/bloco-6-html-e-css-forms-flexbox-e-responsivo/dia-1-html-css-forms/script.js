fee09d21d2fd2a67c4472ec56fb7b42f349735bc
Millene Galetti <mikagaletti1@gmail.com>
2021-09-15 16:00:27 -0300

    Exercicios iniciado

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,10 @@
+// Estado - Select
+// Todos os estados do Brasil
+// Utilize estruturas de repetição via JavaScript para gerar os <option>
+
+// Data de início - Texto
+// Verificar o formato da data dd/mm/aaaa .
+// O dia deve ser > 0 e <= 31.
+// O mês deve ser > 0 e <= 12.
+// O ano não pode ser negativo.
+// Caso alguma das condições seja inválida no momento do envio do formulário, exibir via alert uma mensagem de erro contextualizada.
