9799c72b3252449c3d14d63468ee974884eff7b7
Millene Galetti <mikagaletti1@gmail.com>
2021-09-24 18:28:52 -0300

    exercicio 4

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script2.js
@@ -41,4 +41,40 @@ console.log(longestWord1("Antonio foi no banheiro e não sabemos o que aconteceu
 
 // 4. Crie um código JavaScript com a seguinte especificação:
 // Função 1 : Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string .
+
+// const array = ['Javascript', 'SQL', 'Phyton', 'Html'];
+
+// function searchX (string) {
+      
+// }
 // Função 2 : Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string .
+
+
+// const array = ["Android", "iOS", "Architecture", "Teach", "Run"];
+
+// function buildSkillsPhrase (paramOne) {
+//     const fun1 = paramInner => (
+//       `Tryber ${paramInner} aqui!
+
+//       Tudo bem?`
+//     );
+
+//     let result = `${fun1(paramOne)}
+
+//     Minhas cinco principais habilidades são:`;
+
+//     array.forEach((skill, index) =>
+//     result = `${result}
+
+//     - ${skill}`);
+
+//     result = `
+//     ${result}
+
+//     #goTrybe
+//     `;
+
+//     return result;
+// }
+
+// console.log(buildSkillsPhrase("Lucas"));
