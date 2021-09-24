b5276e196fdc6591c7effbdf7ba9be954849bc6d
Millene Galetti <mikagaletti1@gmail.com>
2021-09-24 18:19:52 -0300

    Exercicio 3

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script2.js
@@ -33,3 +33,12 @@ const longestWord = (frase) => {
     }
 
 console.log(longestWord("Antônio foi no banheiro e não sabemos o que aconteceu")); 
+
+// bônus Sort
+const longestWord1 = text => text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0];
+
+console.log(longestWord1("Antonio foi no banheiro e não sabemos o que aconteceu"));
+
+// 4. Crie um código JavaScript com a seguinte especificação:
+// Função 1 : Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string .
+// Função 2 : Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string .
