34b531dfd7627f4ada28ee250d7408e38c447d8d
Millene Galetti <mikagaletti1@gmail.com>
2021-09-27 11:47:22 -0300

    Exercicio ficar parte 2

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script.js
@@ -0,0 +1,33 @@
+// crie uma função que receba três parâmetros, sendo eles: um objeto, o nome de uma chave e o seu valor. O retorno dessa função deve ser o objeto já com a nova chave adicionada nele.
+
+
+
+// criar uma função que exiba as habilidades do objeto student . Cada habilidade deve ser exibida no formato "Nome da habilidade, Nível: valor da chave referente à habilidade".
+
+const student1 = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkills: 'Ótimo',
+  };
+  
+  const student2 = {
+    Html: 'Bom',
+    Css: 'Ótimo',
+    JavaScript: 'Ruim',
+    SoftSkills: 'Ótimo',
+    Git: 'Bom', 
+  };
+
+const showObject = (student) => {
+    const newHabilit = Object.keys(student);
+    for (const key in newHabilit) {
+        console.log(`${newHabilit[key]}, Nível: ${student[newHabilit[key]]}`);
+    }
+};
+
+console.log('Estudante 1');
+showObject(student1);
+
+console.log('Estudante 2');
+showObject(student2);
