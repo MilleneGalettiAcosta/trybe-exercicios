94789ef80b3f9aed246e47b80acc808a7533eb76
Millene Galetti <mikagaletti1@gmail.com>
2021-09-27 18:06:19 -0300

    Parte 1 - exercicio 1 e 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/script.js
@@ -20,32 +20,32 @@ console.log(newObject({}, 'Nome','Millene'))
 
 // criar uma função que exiba as habilidades do objeto student . Cada habilidade deve ser exibida no formato "Nome da habilidade, Nível: valor da chave referente à habilidade".
 
-// const student1 = {
-//     Html: 'Muito Bom',
-//     Css: 'Bom',
-//     JavaScript: 'Ótimo',
-//     SoftSkills: 'Ótimo',
-//   };
+const student1 = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkills: 'Ótimo',
+  };
   
-//   const student2 = {
-//     Html: 'Bom',
-//     Css: 'Ótimo',
-//     JavaScript: 'Ruim',
-//     SoftSkills: 'Ótimo',
-//     Git: 'Bom', 
-//   };
+  const student2 = {
+    Html: 'Bom',
+    Css: 'Ótimo',
+    JavaScript: 'Ruim',
+    SoftSkills: 'Ótimo',
+    Git: 'Bom', 
+  };
 
-// const showObject = (student) => {
-//     const newHabilit = Object.keys(student);
-//     for (const key in newHabilit) {
-//         console.log(`${newHabilit[key]}, Nível: ${student[newHabilit[key]]}`);
-//     }
-// };
+const showObject = (student) => {
+    const newHabilit = Object.keys(student);
+    for (const key in newHabilit) {
+        console.log(`${newHabilit[key]}, Nível: ${student[newHabilit[key]]}`);
+    }
+};
 
-// console.log('Estudante 1');
-// showObject(student1);
+console.log('Estudante 1');
+showObject(student1);
 
-// console.log('Estudante 2');
-// showObject(student2);
+console.log('Estudante 2');
+showObject(student2);
 
 
