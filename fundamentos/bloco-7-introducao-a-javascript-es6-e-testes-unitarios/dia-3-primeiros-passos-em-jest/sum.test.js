6a0f954b38e354e7d8ab97bfa8a0311945d66137
Millene Galetti <mikagaletti1@gmail.com>
2021-09-28 19:53:20 -0300

    Exercicio 1. 1,2,3.

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -0,0 +1,13 @@
+const sum = require('./sum');
+
+test('sums two values', () => {
+  expect(sum(4, 5)).toBe(9);
+});
+
+test('sums two values', () => {
+  expect(sum(0, 0)).toBe(0);
+});
+
+test('sums two values', () => {
+  expect(sum(4, '5')).toBe(9);
+});
