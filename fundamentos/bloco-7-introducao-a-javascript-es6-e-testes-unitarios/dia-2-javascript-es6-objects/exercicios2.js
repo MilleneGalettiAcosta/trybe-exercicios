0d96b61744a92bb7f2bf9c7dd4c26823a2cce1a9
Millene Galetti <mikagaletti1@gmail.com>
2021-09-28 12:28:12 -0300

    parte 2- exercicio 6 e 7

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/exercicios2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/exercicios2.js
@@ -47,6 +47,36 @@ valueObjects(lesson1);
 
 // Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
 
-const allLessons = Object.assign({}, {lesson1}, {lesson2}, {lesson3});
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
 
 console.log(allLessons);
+
+// Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+
+function totalStudants(objeto) {
+   let total = 0;
+   const array = Object.keys(objeto);
+   for (index in array) {
+       total += objeto[array[index]].numeroEstudantes;
+   }
+   return total;
+};
+
+console.log(totalStudants(allLessons));
+
+// Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto
+
+const getValueByNumber = (objeto, index) => Object.values(objeto)[index];
+
+
+console.log(getValueByNumber(lesson1, 0));
+
+// Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave.
+
+function verifyPair(objeto, chave, valor) {
+   
+}
+
+console.log(verifyPair(lesson3, 'turno', 'noite'));
+
+console.log(verifyPair(lesson3, 'materia', 'Maria Clara'));
