974c1d226acae7fd1c81fce70f3ca4318b23137e
Millene Galetti <mikagaletti1@gmail.com>
2021-10-04 15:43:51 -0300

    Exercicio 1

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -1,14 +1,33 @@
-const wakeUp = () => 'Acordando!!';
+// const wakeUp = () => 'Acordando!!';
 
-const coffee = () => 'Bora tomar café!!';
+// const coffee = () => 'Bora tomar café!!';
 
-const sleep = () => 'Partiu dormir!!';
+// const sleep = () => 'Partiu dormir!!';
 
-const doingThings = (callback) => {
-    const string = callback();
-    console.log(string);
+// const doingThings = (callback) => {
+//     const string = callback();
+//     console.log(string);
+// };
+
+// doingThings(wakeUp);
+// doingThings(coffee);
+// doingThings(sleep);
+
+const newPerson = (fullName) => {
+    const email = fullName.toLowerCase().split(' ').join('_');
+    // transforma nome com letra minúscula, separa e adiciona o _ para formatar o email.
+    return { fullName, email: `${email}@trybe.com` };
 };
 
-doingThings(wakeUp);
-doingThings(coffee);
-doingThings(sleep);
+console.log(newPerson('Millene Galetti'));
+
+const newEmployees = (callback) => {
+    const employees = {
+        id1: callback('Pedro Guerra'),
+        id2: callback('Luiza Drumond'),
+        id3: callback('Carla Paiva'),
+    };
+    return employees;
+};
+
+console.log(newEmployees(newPerson));
