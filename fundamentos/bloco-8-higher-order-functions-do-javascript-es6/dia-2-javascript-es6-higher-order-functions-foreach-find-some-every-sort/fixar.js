ed6ee85c282a198feb655f9a502a82602a0866ff
Millene Galetti <mikagaletti1@gmail.com>
2021-10-05 17:52:08 -0300

    fixar 2.2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixar.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixar.js
@@ -66,3 +66,12 @@ const findDivisibleBy3And5 = (num) => {
 }
 
 console.log(findDivisibleBy3And5(numbers));
+
+// 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = (arr) => {
+  return arr.find((word) => word.length === 5);
+}
+
+console.log(findNameWithFiveLetters(names));
