c4a8fefb18d333089b849684cc9f25427ba00feb
Millene Galetti <mikagaletti1@gmail.com>
2021-10-05 17:50:30 -0300

    fixar 2.1

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixar.js
@@ -0,0 +1,68 @@
+//Exemplo 
+const students = [
+    { name: 'Maria', grade: 70, approved: '' },
+    { name: 'José', grade: 56, approved: '' },
+    { name: 'Roberto', grade: 90, approved: '' },
+    { name: 'Ana', grade: 81, approved: '' },
+  ];
+  
+  function verifyGrades() {
+    students.forEach((student, index) => {
+      if (student.grade >= 60) {
+        students[index].approved = 'Aprovado';
+      } else {
+        students[index].approved = 'Recuperação';
+      }
+    });
+  }
+  
+  verifyGrades();
+  
+  console.log(students);
+  // [
+  //   { name: 'Maria', grade: 70, approved: 'Aprovado' },
+  //   { name: 'José', grade: 56, approved: 'Recuperação' },
+  //   { name: 'Roberto', grade: 90, approved: 'Aprovado' },
+  //   { name: 'Ana', grade: 81, approved: 'Aprovado' }
+  // ]
+
+//Exemplo 2
+// const numbers = [11, 24, 39, 47, 50, 62, 75, 81, 96, 100];
+// const firstMultipleOf5 = numbers.find((number) => number % 5 === 0);
+
+// console.log(firstMultipleOf5);
+// 50
+
+const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+arrayOfValues.forEach((element) => {
+  console.log('Cada elemento do array:', element);
+});
+// // Cada elemento do array: josé
+// Cada elemento do array: 50
+// Cada elemento do array: 0.25
+// Cada elemento do array: { comida: 'Chocolate' }
+
+// 1 - Use o método forEach chamando a callback showEmailList para apresentar os emails
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+const showEmailList = (email) => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+};
+
+emailListInData.forEach((showEmailList) => {
+  console.log(showEmailList);
+});
+
+// 1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5 , caso ele exista:
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = (num) => {
+  return num.find((number) => number % 3 === 0 && number % 5 === 0);
+}
+
+console.log(findDivisibleBy3And5(numbers));
