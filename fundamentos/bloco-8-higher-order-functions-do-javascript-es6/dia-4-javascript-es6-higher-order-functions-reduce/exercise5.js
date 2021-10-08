4a7491284392bfc6e1f398ad1309a6a77d771077
Millene Galetti <mikagaletti1@gmail.com>
2021-10-08 09:21:25 -0300

    exercicio 5

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise5.js
@@ -0,0 +1,18 @@
+// 5 - Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+
+const names = [
+    'Aanemarie', 'Adervandes', 'Akifusa',
+    'Abegildo', 'Adicellia', 'Aladonata',
+    'Abeladerco', 'Adieidy', 'Alarucha',
+  ];
+  
+  function containsA() {
+    return names.reduce((acc, name) =>
+    acc + name.split('')
+    .reduce((acc2, letter) => {
+        if (letter === 'a' || letter === 'A') return acc2 + 1;
+        return acc2;
+    },0),0);
+  }
+  
+  console.log(containsA());
