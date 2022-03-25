e86d3a41467471994d355406fe344828b359a6a9
Millene Galetti <mikagaletti1@gmail.com>
2022-03-24 22:42:21 -0300

    Refact: Refatorando codigo books

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/fixar/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/fixar/index.js
@@ -13,15 +13,31 @@ app.get('/authors', async (_req, res) => {
   res.status(200).json(authors);
 });
 
-app.get('/books', async (_req, res) => {
-  const books = await Book.getAll();
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const author = await Author.findById(id);
+
+  if (!author) return res.status(404).json({ message: 'Not found' });
+
+  res.status(200).json(author);
+});
+
+app.get('/books', async (req, res) => {
+  const { author_id } = req.query;
+
+  const books = (author_id) ? await Book.getAuthorById(author_id) : await Book.getAll();
 
   res.status(200).json(books);
 });
 
-app.get('/books/:author_id', async (req, res) => {
-  const { author_id } = req.params;
-  const book = await Book.getAuthorById(author_id);
+app.get('./books/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const book = await Book.getBookById(id);
+
+  if (!book) return res.status(404).json({ message: 'Not found' });
+
   res.status(200).json(book);
 });
 
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/fixar/models/Books.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/fixar/models/Books.js
@@ -1,19 +1,38 @@
 const connection = require('./connection');
 
-const serialize = (bookData) => ({
-  id: bookData.id,
-  tittle: bookData.title,
-  authorID: bookData.author_id,
-});
 
 const getAll = async () => {
-  const [ books ] = await connection.execute('SELECT id, title, author_id FROM model_example.books;',);
-  return books.map(serialize);
+  const [ books ] = await connection.execute('SELECT * FROM model_example.books;');
+  return books.map(({ id, title, author_id }) => ({
+    id,
+    title,
+    authorId: author_id,
+  }));
 }
 
 const getAuthorById= async (author_id) => {
-  const [ book ] = await connection.execute('SELECT id, title, author_id FROM model_example.books WHERE author_id = ?', [author_id]);
-  return book.map(serialize);
+  const query = 'SELECT * FROM model_example.books WHERE author_id=?;'
+  const [books] = await connection.execute(query, [ author_id ]);
+
+  return books.map(({ id, title, author_id }) => ({
+    id,
+    title,
+    authorId: author_id,
+  }));
 }
 
-module.exports = { getAll , getAuthorById, };
+const getBookById = async (id) => {
+  const query = 'SELECT * FROM model_example.books WHERE id=?;'
+
+  const [books] = await connection.execute(query, [id]);
+
+  if (books.length === 0) return null;
+
+  return books.map(({ id, title, author_id }) => ({
+    id,
+    title,
+    authorId: author_id,
+  }))[0];
+}
+
+module.exports = { getAll , getAuthorById, getBookById };
