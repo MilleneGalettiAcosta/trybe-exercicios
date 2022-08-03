9a5d65d4b5300d5e9a3c3ef6a1e567821300cf44
Millene Galetti <mikagaletti1@gmail.com>
2022-08-03 09:35:27 -0300

    Feat: Enunciado Ex 02

--- ciencia-da-computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/ex2.py
+++ ciencia-da-computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/ex2.py
@@ -1,3 +1,19 @@
+# Jogo da palavra embaralhada.
+
+# Desenvolva um jogo em que a pessoa usuária tenha que adivinhar
+# uma palavra que será mostrada com as letras embaralhadas.
+
+# O programa terá uma lista de palavras e escolherá uma aleatoriamente.
+# O jogador terá três tentativas para adivinhar a palavra. Ao final,
+# a palavra deve ser mostrada na tela,
+# informando se a pessoa ganhou ou perdeu o jogo.
+
+# Para embaralhar uma palavra faça:
+# scrambled_word = "".join(random.sample(word, len(word)))
+
+# O sorteio de uma palavra aleatória pode ser feito utilizando o método choice:
+# random.choice(["word1", "word2", "word3"]) -> "word2".
+
 import random
 
 WORDS = [
